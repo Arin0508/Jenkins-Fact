bae97280d24cecaa17109f796802e1a4f9de5032
Arin0508 <77500921+Arin0508@users.noreply.github.com>
2022-11-25 11:57:19 +0530

    Updated test cases

--- testerWro.py
+++ testerWro.py
@@ -4,10 +4,10 @@ from factorial import fact
 
 class TestCase(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(fact(-1),0)
+        self.assertEqual(fact(-1),-1)
 
     def test_case2(self):
-        self.assertEqual(fact(-10),0)
+        self.assertEqual(fact(-10),-1)
 
 
 if __name__ == '__main__':
