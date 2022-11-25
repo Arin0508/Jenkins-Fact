471fb70a3cba9c3311c7d08ce29ce90128608fbc
Arin0508 <77500921+Arin0508@users.noreply.github.com>
2022-11-25 11:56:45 +0530

    Final Update for fact

--- factorial.py
+++ factorial.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python3
 def fact(n):
-	res = 1	
-	for i in range(1,n+1):
-		res = res * i
-	return res
+    #Expected output is -1 ... but I have changed it to 1 to get wrong ans in test cases
+    if (n < 0):
+        return 1
+    else :
+        res = 1	
+        for i in range(1,n+1):
+            res = res * i
+        return res
 
 #Driver code
 t = 10
